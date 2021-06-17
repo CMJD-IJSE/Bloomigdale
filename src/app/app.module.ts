e56f2667d3fb554d1dee2402bef89f48e60e9e48
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-06-17 10:56:40 +0530

    Added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatInputModule} from '@angular/material/input';
 import {CarouselModule} from 'ngx-owl-carousel-o';
 import {MatButtonModule} from '@angular/material/button';
-import { RecommendedCarouselComponent } from './components/recommended-carousel/recommended-carousel.component';
-import { BestGiftsCarouselComponent } from './components/best-gifts-carousel/best-gifts-carousel.component';
-import { NewArrivalCarouselComponent } from './components/new-arrival-carousel/new-arrival-carousel.component';
+import {RecommendedCarouselComponent} from './components/recommended-carousel/recommended-carousel.component';
+import {BestGiftsCarouselComponent} from './components/best-gifts-carousel/best-gifts-carousel.component';
+import {NewArrivalCarouselComponent} from './components/new-arrival-carousel/new-arrival-carousel.component';
 
 @NgModule({
   declarations: [
@@ -31,10 +31,11 @@ import { NewArrivalCarouselComponent } from './components/new-arrival-carousel/n
     MatDividerModule,
     MatInputModule,
     CarouselModule,
-    MatButtonModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
 

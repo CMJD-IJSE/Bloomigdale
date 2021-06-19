818f07e1796af130d585a6b70f99e08a02c1d88f
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-06-19 13:00:10 +0530

    Added bootstrap table

--- src/app/components/share/alert-ui/alert-ui.component.ts
+++ src/app/components/share/alert-ui/alert-ui.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, ViewChild} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {itemData} from '../ItemData';
 
@@ -13,21 +13,6 @@ export class AlertUIComponent implements OnInit {
     public dialogRef: MatDialogRef<AlertUIComponent>,
     @Inject(MAT_DIALOG_DATA) public data: itemData[]) {}
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  ELEMENT_DATA: PeriodicElement[] = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  ];
-
-  dataSource = this.ELEMENT_DATA;
 
   onNoClick(): void {
     this.dialogRef.close();
@@ -36,11 +21,4 @@ export class AlertUIComponent implements OnInit {
   ngOnInit(): void {
   }
 
-}
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
 }
